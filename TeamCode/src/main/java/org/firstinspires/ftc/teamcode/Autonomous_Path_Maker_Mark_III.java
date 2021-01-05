b249bc0ac7c7ee78fcd6402dc698dddce94171c7
iloveusername <bchasestair@gmail.com>
2021-01-04 22:12:03 -0800

    Update Autonomous_Path_Maker_Mark_III.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Path_Maker_Mark_III.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Path_Maker_Mark_III.java
@@ -31,8 +31,13 @@ public class Autonomous_Path_Maker_Mark_III extends LinearOpMode {
         BrightDrive = hardwareMap.get(DcMotor.class, "BR");
 
         while (opModeIsActive()) {
+
+            //Put the movement stuff here.
             goToTarget(1,1,0.5);
-            goToCoordinates(-1, -1, 0);
+            goToCoordinates(-1, -1, 0.5);
+            goToOrigin(0.5);
+            rotateToAngle(0, 0.5);
+            stop();
         }
     }
 
@@ -126,193 +131,6 @@ public class Autonomous_Path_Maker_Mark_III extends LinearOpMode {
         currentAngle = AngleOfTri;
         currentX += targetX;
         currentY += targetY;
-
-        //Telemetry stuff for debugging.
-        telemetry.addData("Angle Of Attack", AngleOfTri);
-        telemetry.addData("Hypotenuse", HypotenuseOfTri);
-        telemetry.addData("Quadrant", Quadrant);
-        telemetry.addData("Current X", currentX);
-        telemetry.addData("Current Y", currentY);
-        telemetry.addData("Current Rot", currentAngle);
-        telemetry.update();
-
-    }
-
-    public void encoderDrive(int desiredEncoder, double desiredSpeed){
-        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        BleftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        BrightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        BleftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        BrightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        leftDrive.setTargetPosition(desiredEncoder);
-        rightDrive.setTargetPosition(desiredEncoder);
-        BleftDrive.setTargetPosition(desiredEncoder);
-        BrightDrive.setTargetPosition(desiredEncoder);
-
-        leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        BleftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        BrightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        leftDrive.setPower(desiredSpeed);
-        rightDrive.setPower(desiredSpeed);
-        BleftDrive.setPower(desiredSpeed);
-        BrightDrive.setPower(desiredSpeed);
-
-        while(leftDrive.isBusy() || rightDrive.isBusy() || BleftDrive.isBusy() || BrightDrive.isBusy()){
-        }
-
-        leftDrive.setPower(0);
-        rightDrive.setPower(0);
-        BleftDrive.setPower(0);
-        BrightDrive.setPower(0);
-
-        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        BleftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        BrightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    }
-
-    public void wheelDirection(String dir){
-        switch (dir){
-            case "up":
-                leftDrive.setDirection(DcMotor.Direction.FORWARD);
-                rightDrive.setDirection(DcMotor.Direction.REVERSE);
-                BleftDrive.setDirection(DcMotor.Direction.FORWARD);
-                BrightDrive.setDirection(DcMotor.Direction.REVERSE);
-                break;
-            case "down":
-                leftDrive.setDirection(DcMotor.Direction.REVERSE);
-                rightDrive.setDirection(DcMotor.Direction.FORWARD);
-                BleftDrive.setDirection(DcMotor.Direction.REVERSE);
-                BrightDrive.setDirection(DcMotor.Direction.FORWARD);
-                break;
-            case "left":
-                leftDrive.setDirection(DcMotor.Direction.REVERSE);
-                rightDrive.setDirection(DcMotor.Direction.FORWARD);
-                BleftDrive.setDirection(DcMotor.Direction.FORWARD);
-                BrightDrive.setDirection(DcMotor.Direction.REVERSE);
-                break;
-            case "right":
-                leftDrive.setDirection(DcMotor.Direction.FORWARD);
-                rightDrive.setDirection(DcMotor.Direction.REVERSE);
-                BleftDrive.setDirection(DcMotor.Direction.REVERSE);
-                BrightDrive.setDirection(DcMotor.Direction.FORWARD);
-                break;
-            case "turnLeft":
-                leftDrive.setDirection(DcMotor.Direction.REVERSE);
-                rightDrive.setDirection(DcMotor.Direction.REVERSE);
-                BleftDrive.setDirection(DcMotor.Direction.REVERSE);
-                BrightDrive.setDirection(DcMotor.Direction.REVERSE);
-                break;
-            case "turnRight":
-                leftDrive.setDirection(DcMotor.Direction.FORWARD);
-                rightDrive.setDirection(DcMotor.Direction.FORWARD);
-                BleftDrive.setDirection(DcMotor.Direction.FORWARD);
-                BrightDrive.setDirection(DcMotor.Direction.FORWARD);
-                break;
-        }
-    }
-
-    public void goToOrigin(double targetSpeed){
-        //Sets up a switch to toggle this function on and off with.
-        boolean isDone = false;
-
-        //If we take our current position, then input the X and Y values into the trig stuff, then just go backwards instead of forwards, we go straight back to origin. Thanks, geometry.
-        double targetX = currentX;
-        double targetY = currentY;
-
-        //With Absolute Values, We Can Find The Angles We Want Without Crying. No promises.
-        double adjustedX = Math.abs(targetX);
-        double adjustedY = Math.abs(targetY);
-        int Quadrant = 1;
-
-        //This Determines The Quadrant Of The Angle.
-        if(targetX > 0){
-            if(targetY > 0){
-                Quadrant = 1;
-            }
-            if(targetY < 0){
-                Quadrant = 4;
-            }
-        }
-        if(targetX < 0){
-            if(targetY > 0){
-                Quadrant = 2;
-            }
-            if(targetY < 0){
-                Quadrant = 3;
-            }
-        }
-
-        //Finds Hypotenuse and the Theta value of the triangle.
-        double HypotenuseOfTri = Math.sqrt((targetX * targetX) + (targetY * targetY));
-        double AngleOfTri = Math.atan(adjustedY/adjustedX) * 180/Math.PI;
-
-        //How much we need to turn depends on the quadrant we are currently in.
-        switch (Quadrant){
-            case 1:
-                AngleOfTri = 90 - AngleOfTri;
-                break;
-            case 2:
-                AngleOfTri = -90 + AngleOfTri;
-                break;
-            case 3:
-                AngleOfTri = -90 - AngleOfTri;
-                break;
-            case 4:
-                AngleOfTri = 90 + AngleOfTri;
-                break;
-        }
-
-        //This determines if we are going vertical or horizontal, and sets the angle to whatever it needs to be.
-        if(targetX == 0){
-            if(targetY != 0){
-                HypotenuseOfTri = targetY;
-                if(targetY > 0){
-                    AngleOfTri = 0;
-                }
-                if(targetY < 0){
-                    AngleOfTri = -180;
-                }
-            }
-        }
-        if(targetY == 0){
-            if(targetX != 0){
-                HypotenuseOfTri = targetX;
-                if(targetX > 0){
-                    AngleOfTri = 90;
-                }
-                if(targetX < 0){
-                    AngleOfTri = -90;
-                }
-            }
-        }
-
-        //Determine turning direction.
-        if(AngleOfTri > currentAngle){
-            wheelDirection("turnRight");
-        }
-        if(AngleOfTri < currentAngle){
-            wheelDirection("turnLeft");
-        }
-
-        //Turns the robot using encoders for accuracy. Adjust speed if you want.
-        encoderDrive((int) Math.round((AngleOfTri*rotToEncoder) - (currentAngle*rotToEncoder)), 0.5);
-
-        //Moves the robot forward for the distance of the hypotenuse.
-        wheelDirection("down");
-        encoderDrive((int) Math.round(HypotenuseOfTri * meterToEncoder), targetSpeed);
-
-        //Updates current position and rotation.
-        currentAngle = AngleOfTri;
-        currentX = 0;
-        currentY = 0;
 
         //Telemetry stuff for debugging.
         telemetry.addData("Angle Of Attack", AngleOfTri);
@@ -431,6 +249,112 @@ public class Autonomous_Path_Maker_Mark_III extends LinearOpMode {
 
     }
 
+    public void goToOrigin(double targetSpeed){
+        //Sets up a switch to toggle this function on and off with.
+        boolean isDone = false;
+
+        //If we take our current position, then input the X and Y values into the trig stuff, then just go backwards instead of forwards, we go straight back to origin. Thanks, geometry.
+        double targetX = currentX;
+        double targetY = currentY;
+
+        //With Absolute Values, We Can Find The Angles We Want Without Crying. No promises.
+        double adjustedX = Math.abs(targetX);
+        double adjustedY = Math.abs(targetY);
+        int Quadrant = 1;
+
+        //This Determines The Quadrant Of The Angle.
+        if(targetX > 0){
+            if(targetY > 0){
+                Quadrant = 1;
+            }
+            if(targetY < 0){
+                Quadrant = 4;
+            }
+        }
+        if(targetX < 0){
+            if(targetY > 0){
+                Quadrant = 2;
+            }
+            if(targetY < 0){
+                Quadrant = 3;
+            }
+        }
+
+        //Finds Hypotenuse and the Theta value of the triangle.
+        double HypotenuseOfTri = Math.sqrt((targetX * targetX) + (targetY * targetY));
+        double AngleOfTri = Math.atan(adjustedY/adjustedX) * 180/Math.PI;
+
+        //How much we need to turn depends on the quadrant we are currently in.
+        switch (Quadrant){
+            case 1:
+                AngleOfTri = 90 - AngleOfTri;
+                break;
+            case 2:
+                AngleOfTri = -90 + AngleOfTri;
+                break;
+            case 3:
+                AngleOfTri = -90 - AngleOfTri;
+                break;
+            case 4:
+                AngleOfTri = 90 + AngleOfTri;
+                break;
+        }
+
+        //This determines if we are going vertical or horizontal, and sets the angle to whatever it needs to be.
+        if(targetX == 0){
+            if(targetY != 0){
+                HypotenuseOfTri = targetY;
+                if(targetY > 0){
+                    AngleOfTri = 0;
+                }
+                if(targetY < 0){
+                    AngleOfTri = -180;
+                }
+            }
+        }
+        if(targetY == 0){
+            if(targetX != 0){
+                HypotenuseOfTri = targetX;
+                if(targetX > 0){
+                    AngleOfTri = 90;
+                }
+                if(targetX < 0){
+                    AngleOfTri = -90;
+                }
+            }
+        }
+
+        //Determine turning direction.
+        if(AngleOfTri > currentAngle){
+            wheelDirection("turnRight");
+        }
+        if(AngleOfTri < currentAngle){
+            wheelDirection("turnLeft");
+        }
+
+        //Turns the robot using encoders for accuracy. Adjust speed if you want.
+        encoderDrive((int) Math.round((AngleOfTri*rotToEncoder) - (currentAngle*rotToEncoder)), 0.5);
+
+        //Moves the robot forward for the distance of the hypotenuse.
+        wheelDirection("down");
+        encoderDrive((int) Math.round(HypotenuseOfTri * meterToEncoder), targetSpeed);
+
+        //Updates current position and rotation.
+        currentAngle = AngleOfTri;
+        currentX = 0;
+        currentY = 0;
+
+        //Telemetry stuff for debugging.
+        telemetry.addData("Angle Of Attack", AngleOfTri);
+        telemetry.addData("Hypotenuse", HypotenuseOfTri);
+        telemetry.addData("Quadrant", Quadrant);
+        telemetry.addData("Current X", currentX);
+        telemetry.addData("Current Y", currentY);
+        telemetry.addData("Current Rot", currentAngle);
+        telemetry.update();
+
+    }
+
     public void rotateToAngle(double angle, double desiredSpeed){
 
         //Determine if we need to spin left or right.
@@ -445,6 +369,93 @@ public class Autonomous_Path_Maker_Mark_III extends LinearOpMode {
         encoderDrive((int) Math.round((angle*rotToEncoder) - (currentAngle*rotToEncoder)), desiredSpeed);
 
     }
+
+    public void wheelDirection(String dir){
+        switch (dir){
+            case "up":
+                leftDrive.setDirection(DcMotor.Direction.FORWARD);
+                rightDrive.setDirection(DcMotor.Direction.REVERSE);
+                BleftDrive.setDirection(DcMotor.Direction.FORWARD);
+                BrightDrive.setDirection(DcMotor.Direction.REVERSE);
+                break;
+            case "down":
+                leftDrive.setDirection(DcMotor.Direction.REVERSE);
+                rightDrive.setDirection(DcMotor.Direction.FORWARD);
+                BleftDrive.setDirection(DcMotor.Direction.REVERSE);
+                BrightDrive.setDirection(DcMotor.Direction.FORWARD);
+                break;
+            case "left":
+                leftDrive.setDirection(DcMotor.Direction.REVERSE);
+                rightDrive.setDirection(DcMotor.Direction.FORWARD);
+                BleftDrive.setDirection(DcMotor.Direction.FORWARD);
+                BrightDrive.setDirection(DcMotor.Direction.REVERSE);
+                break;
+            case "right":
+                leftDrive.setDirection(DcMotor.Direction.FORWARD);
+                rightDrive.setDirection(DcMotor.Direction.REVERSE);
+                BleftDrive.setDirection(DcMotor.Direction.REVERSE);
+                BrightDrive.setDirection(DcMotor.Direction.FORWARD);
+                break;
+            case "turnLeft":
+                leftDrive.setDirection(DcMotor.Direction.REVERSE);
+                rightDrive.setDirection(DcMotor.Direction.REVERSE);
+                BleftDrive.setDirection(DcMotor.Direction.REVERSE);
+                BrightDrive.setDirection(DcMotor.Direction.REVERSE);
+                break;
+            case "turnRight":
+                leftDrive.setDirection(DcMotor.Direction.FORWARD);
+                rightDrive.setDirection(DcMotor.Direction.FORWARD);
+                BleftDrive.setDirection(DcMotor.Direction.FORWARD);
+                BrightDrive.setDirection(DcMotor.Direction.FORWARD);
+                break;
+        }
+    }
+
+    public void encoderDrive(int desiredEncoder, double desiredSpeed){
+        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BleftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BrightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BleftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BrightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        leftDrive.setTargetPosition(desiredEncoder);
+        rightDrive.setTargetPosition(desiredEncoder);
+        BleftDrive.setTargetPosition(desiredEncoder);
+        BrightDrive.setTargetPosition(desiredEncoder);
+
+        leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BleftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BrightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        leftDrive.setPower(desiredSpeed);
+        rightDrive.setPower(desiredSpeed);
+        BleftDrive.setPower(desiredSpeed);
+        BrightDrive.setPower(desiredSpeed);
+
+        while(leftDrive.isBusy() || rightDrive.isBusy() || BleftDrive.isBusy() || BrightDrive.isBusy()){
+        }
+
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        BleftDrive.setPower(0);
+        BrightDrive.setPower(0);
+
+        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BleftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BrightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+
+
+
+
+
 }
 
 
