d4a7b7283fd4bc98894985b84ff258808048a803
iloveusername <71989017+iloveusername@users.noreply.github.com>
2021-03-15 17:18:50 -0700

    Create TankeDriveTest2021.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TankeDriveTest2021.java
@@ -0,0 +1,128 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.robot.RobotState;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@TeleOp(name="TeleOp Tank Drive", group="Basic")
+public class TankeDriveTest2021 extends LinearOpMode {
+    //Motors
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    private DcMotor BleftDrive = null;
+    private DcMotor BrightDrive = null;
+
+    //This is the onboard gyroscope, pretty neat.
+    BNO055IMU imu;
+    Orientation angles;
+
+    //Sets up odometry.
+    double currentAngle = 0;
+    double currentX = 0;
+    double currentY = 0;
+
+    //Gamepad Stuff
+    double stickL;
+    double stickR;
+
+
+    @Override
+    public void runOpMode(){
+        //This sets up the gryoscope for use.
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        waitForStart();
+
+        //Set up the motors for use.
+        leftDrive = hardwareMap.get(DcMotor.class, "FL");
+        rightDrive = hardwareMap.get(DcMotor.class, "FR");
+        BleftDrive = hardwareMap.get(DcMotor.class, "BL");
+        BrightDrive = hardwareMap.get(DcMotor.class, "BR");
+
+        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BleftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BrightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BleftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BrightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BleftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BrightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        wheelDirection("up");
+
+        while (opModeIsActive()){
+            stickL = -gamepad1.left_stick_y;
+            stickR = -gamepad1.right_stick_y;
+
+            leftDrive.setPower(stickL);
+            BleftDrive.setPower(stickL);
+            rightDrive.setPower(stickR);
+            BrightDrive.setPower(stickR);
+
+            telemetry.addData("FL Encoder", leftDrive.getCurrentPosition());
+            telemetry.addData("FR Encoder", rightDrive.getCurrentPosition());
+            telemetry.update();
+        }
+
+    }
+
+    public void wheelDirection(String dir){
+        switch (dir){
+            case "up":
+                leftDrive.setDirection(DcMotor.Direction.REVERSE);
+                rightDrive.setDirection(DcMotor.Direction.FORWARD);
+                BleftDrive.setDirection(DcMotor.Direction.REVERSE);
+                BrightDrive.setDirection(DcMotor.Direction.FORWARD);
+                break;
+            case "down":
+                leftDrive.setDirection(DcMotor.Direction.FORWARD);
+                rightDrive.setDirection(DcMotor.Direction.REVERSE);
+                BleftDrive.setDirection(DcMotor.Direction.FORWARD);
+                BrightDrive.setDirection(DcMotor.Direction.REVERSE);
+                break;
+            case "left":
+                leftDrive.setDirection(DcMotor.Direction.FORWARD);
+                rightDrive.setDirection(DcMotor.Direction.FORWARD);
+                BleftDrive.setDirection(DcMotor.Direction.REVERSE);
+                BrightDrive.setDirection(DcMotor.Direction.REVERSE);
+                break;
+            case "right":
+                leftDrive.setDirection(DcMotor.Direction.REVERSE);
+                rightDrive.setDirection(DcMotor.Direction.REVERSE);
+                BleftDrive.setDirection(DcMotor.Direction.FORWARD);
+                BrightDrive.setDirection(DcMotor.Direction.FORWARD);
+                break;
+            case "turnLeft":
+                leftDrive.setDirection(DcMotor.Direction.FORWARD);
+                rightDrive.setDirection(DcMotor.Direction.FORWARD);
+                BleftDrive.setDirection(DcMotor.Direction.FORWARD);
+                BrightDrive.setDirection(DcMotor.Direction.FORWARD);
+                break;
+            case "turnRight":
+                leftDrive.setDirection(DcMotor.Direction.REVERSE);
+                rightDrive.setDirection(DcMotor.Direction.REVERSE);
+                BleftDrive.setDirection(DcMotor.Direction.REVERSE);
+                BrightDrive.setDirection(DcMotor.Direction.REVERSE);
+                break;
+        }
+    }
+}
